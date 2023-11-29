e08091dea73ced6952cde19b77b88774c41257fe
Max Siryk <erotourtes@gmail.com>
2023-11-29 17:25:29 +0200

    added: prisma exception filter

---
+++ src/exception-filter/PrismaExceptionFilter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+import { Response } from 'express';
+
+@Catch(PrismaClientKnownRequestError)
+export class PrismaExceptionFilter
+  implements ExceptionFilter<PrismaClientKnownRequestError>
+{
+  private readonly prismaClientKnownRequestErrorCodes = {
+    P2002: HttpStatus.CONFLICT,
+    P2025: HttpStatus.FORBIDDEN,
+  };
+
+  catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status =
+      this.prismaClientKnownRequestErrorCodes[exception.code] ||
+      HttpStatus.INTERNAL_SERVER_ERROR;
+    response.status(status).json({
+      statusCode: status,
+      message: "Can't process your request",
+      error: exception.code,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'process';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './exception-filter/PrismaExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(env.PORT || 4000);
 }
 bootstrap();
