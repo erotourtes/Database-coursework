14c75d87aca503f44155d2c4f7316f689a6a7c01
Max Siryk <erotourtes@gmail.com>
2023-11-29 17:13:21 +0200

    refactored: removed redundant modules & tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { AppController } from './app.controller';
 import { AccessModule } from './access/access.module';
 import { PostModule } from './post/post.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, AccessModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
